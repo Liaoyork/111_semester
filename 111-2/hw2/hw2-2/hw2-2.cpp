65b28b8af365b827e6dbb4847edc4e2f53733ed7
root <root@LAPTOP-NRV75KVB>
2023-05-10 17:28:39 +0800

    0510-1

--- 111-2/hw2/hw2-2/hw2-2.cpp
+++ 111-2/hw2/hw2-2/hw2-2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int Role::attacking() {
     cout << "error!" << endl;
+    return 0;
 }
 
 void Role::subHP_V(int a) {
---
+++ 111-2/python/test.cpp
@@ -0,0 +1,7 @@
+print("Hello, world!")
+
+d = [1,"24",5,'4', "york", "shame"]
+
+for i in range(0,6):
+    print(d[i], end = " ")
+
