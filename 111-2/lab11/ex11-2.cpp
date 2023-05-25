345037d34c65bf699139e4f35c17e83a200d9262
Liaoyork <127830853+Liaoyork@users.noreply.github.com>
2023-05-25 17:26:41 +0800

    0525-1

--- 111-2/lab11/ex11-2.cpp
+++ 111-2/lab11/ex11-2.cpp
@@ -1,7 +1,24 @@
+#include <fstream>
 #include <iostream>
 #include <map>
+#include <string>
 #include <vector>
 using namespace std;
 
+bool rec(string ss, map<string, int> dic) {
+}
+
 int main(int argc, char *argv[]) {
+    map<string, int> dictionary;
+    ifstream in(argv[1]);
+    string ss;
+    int amo;
+    while (in >> ss >> amo) {
+        dictionary.insert(pair<string, int>(ss, amo));
+    }
+    string testcase = argv[2];
+    if (rec(testcase, dictionary)) {
+        for (auto p = dictionary.begin(); p != dictionary.end(); p++) {
+        }
+    }
 }
