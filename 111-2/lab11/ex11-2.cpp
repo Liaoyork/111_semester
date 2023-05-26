fbbabba2f1a558a9a329864218c20fec6d720f42
Liaoyork <127830853+Liaoyork@users.noreply.github.com>
2023-05-26 11:15:31 +0800

    0526-1

--- 111-2/lab11/ex11-2.cpp
+++ 111-2/lab11/ex11-2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -5,20 +6,35 @@
 #include <vector>
 using namespace std;
 
-bool rec(string ss, map<string, int> dic) {
-}
+map<string, int> ori_num;
+vector<string> ans;
 
+bool rec(int start, int end, string ss, map<string, int> dic) {
+}
+bool sortStr(string &s1, string &s2) {
+    return s1 > s2;
+}
 int main(int argc, char *argv[]) {
     map<string, int> dictionary;
     ifstream in(argv[1]);
     string ss;
     int amo;
     while (in >> ss >> amo) {
-        dictionary.insert(pair<string, int>(ss, amo));
+        dictionary.insert(pair<string, int>(ss, 0));
+        ori_num.insert(pair<string, int>(ss, amo));
     }
     string testcase = argv[2];
-    if (rec(testcase, dictionary)) {
+    if (rec(0, 0, testcase, dictionary)) {
+        cout << "True\n";
         for (auto p = dictionary.begin(); p != dictionary.end(); p++) {
+            cout << p->first << ": " << p->second << "(" << ori_num[p->first] << "->" << ori_num[p->first] - p->second << ")\n";
         }
+        sort(ans.begin(), ans.end(), sortStr);
+        for (int i = 0; i < (int)ans.size(); i++) {
+            cout << ans.at(i) << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "False" << endl;
     }
 }
